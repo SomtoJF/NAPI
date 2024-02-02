397a1aa4f61218f4ddba04af6dd140b812e67635
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-02-02 23:43:24 +0100

    Write signup route business logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signup({ email, password }: AuthDto) {
+    // hash the password
+    const hash = await argon.hash(password);
+
+    // save user in DB
+    const response = await this.prisma.user.create({
+      data: { email, hash },
+    });
+    // return new user
+    return response;
+  }
+
+  login() {
+    return 'I am logged in';
+  }
+}
