37b1bf43db344eacfdf979ff22888acee345de12
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-02-03 13:43:35 +0100

    handle unique attribute errors

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,24 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async signup({ email, password }: AuthDto) {
     // hash the password
     const hash = await argon.hash(password);
 
     // save user in DB
-    const response = await this.prisma.user.create({
-      data: { email, hash },
-    });
+    try {
+      const response = await this.prisma.user.create({
+        data: { email, hash },
+      });
 
-    delete response.hash;
-    // return new user
-    return response;
+      delete response.hash;
+      // return new user
+      return response;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if ((error.code = 'P2002')) {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 
   login() {
