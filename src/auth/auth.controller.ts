87d513978b38d9dcfee616e0bbe326cfcd240a45
Francis Somtochukwu <somtofrancis5@gmail.com>
2024-02-05 14:35:50 +0100

    Make auth routes public with custom decorators

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
+import { Public } from '../decorators/public';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) { }
 
+  @Public()
   @Post('signup')
   signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
   }
 
+  @Public()
   @Post('login')
   login(@Body() dto: AuthDto) {
     return this.authService.login(dto);
